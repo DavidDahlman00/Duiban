a807698a08db60bbfe820db7662d1952d8acd360
david <daviddahlman82@gmail.com>
2021-11-15 14:14:52 +0100

    auth login

--- app/src/main/java/com/example/duiban/LoginActivity.kt
+++ app/src/main/java/com/example/duiban/LoginActivity.kt
@@ -3,9 +3,16 @@ package com.example.duiban
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+    lateinit var email: EditText
+    lateinit var password: EditText
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
